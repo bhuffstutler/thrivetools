46230b17a09ed46388ba57b22746bf3ad15cdc7e
Brandon Huffstutler <bhuffstutler@gmail.com>
2016-09-21 16:43:33 -0500

    Blade setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="/favicon.ico">
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-    <title>Fixed Top Navbar Example for Bootstrap</title>
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <title>@yield('title', 'ThriveTools')</title>
 </head>
 
 <body>
